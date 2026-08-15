4b955ebc585a6353e58ec28fae34b02199b0bef3
Alexander Liu <alexander.liu22@outlook.com>
2026-08-15 10:26:10 -0400

    [3090][easy] add dynamic sliding window soln

---
+++ 3090-[EASY]-Maximum-Length-Substring-With-Two-Occurrences.py
@@ -0,0 +1,39 @@
+class Solution:
+    """
+    Intuition:
+        Dynamic sliding window problem with a frequency counter to track the
+        condition.
+
+    Runtime:
+        O(n) as each index in the string is processed at most twice (once by
+        right, once by left ptrs).
+
+    Memory:
+        O(1) for pointers and bounded array.
+    """
+
+    def maximumLengthSubstring(self, s: str) -> int:
+        l, r = 0, 0
+        res = 0
+        freq = [0] * 26
+
+        while r < len(s):
+            # update freq counter w curr char
+            ix = ord(s[r]) - ord('a')
+            freq[ix] += 1
+
+            # check window validity and resize window if needed
+            while freq[ix] > 2:
+                jx = ord(s[l]) - ord('a')
+                freq[jx] -= 1
+                l += 1
+
+            # store res
+            size = r - l + 1
+            if size > res:
+                res = size
+
+            # increment r
+            r += 1
+
+        return res
