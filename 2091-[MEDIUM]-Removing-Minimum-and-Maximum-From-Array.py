ebb3ca18e99eea49ffebb8c24cb6feefb96b5c3a
Alexander Liu <alexander.liu22@outlook.com>
2026-08-29 21:29:04 -0400

    [2091][medium] add slightly more optimized soln

--- 2091-[MEDIUM]-Removing-Minimum-and-Maximum-From-Array.py
+++ 2091-[MEDIUM]-Removing-Minimum-and-Maximum-From-Array.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         There are 3 scenarios in which we can perform all the deletions:
@@ -31,6 +31,43 @@ class Solution:
                 minIx = i
             if nums[i] > nums[maxIx]:
                 maxIx = i
+
+        # we don't care which index represents min or max,
+        # just care about which one is left or right boundary
+        l = min(minIx, maxIx)
+        r = max(minIx, maxIx)
+
+        return min(
+            # just from front
+            r + 1,
+            # just from back
+            len(nums) - l,
+            # both sides
+            (l + 1) + (len(nums) - r),
+        )
+
+
+class Solution2:
+    """
+    Intuition:
+        Same intuition as solution 1, just added an early short-ciruit
+        to slightly optimize runtime.
+
+    Runtime:
+        Same as solution 1.
+
+    Memory:
+        Same as solution 1.
+    """
+
+    def minimumDeletions(self, nums: List[int]) -> int:
+        # find indices of elmts of interest
+        minElmt, maxElmt = min(nums), max(nums)
+        # early short circuit base case
+        if minElmt == maxElmt:
+            return 1
+
+        minIx, maxIx = nums.index(minElmt), nums.index(maxElmt)
 
         # we don't care which index represents min or max,
         # just care about which one is left or right boundary
