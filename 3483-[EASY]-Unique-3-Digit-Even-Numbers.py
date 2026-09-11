af4560ddb21569495e2f4bdc73f43097455edf08
Alexander Liu <alexander.liu22@outlook.com>
2026-09-11 12:00:10 -0400

    [3483][easy] add alternative soln

--- 3483-[EASY]-Unique-3-Digit-Even-Numbers.py
+++ 3483-[EASY]-Unique-3-Digit-Even-Numbers.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         Brute force approach. We form all valid 3-digit numbers and
@@ -36,5 +36,40 @@ class Solution:
                     if num % 2 == 0 and num not in seen:
                         seen.add(num)
                         res += 1
+
+        return res
+
+
+class Solution2:
+    """
+    Intuition:
+        Instead of trying to brute force all 3-digit numbers from the input, we
+        can try scanning through all possible 3-digit numbers that are even (search
+        space small enough -- 450 nums to be exact).
+
+        We iterate through each candidate and see if we can form it with the input
+        we have.
+
+    Runtime:
+        O(n) to count the frequency.
+
+        O(450) ~ O(1) to scan each candidate.
+
+        Overall, O(n) runtime.
+
+    Memory:
+        O(10) ~ O(1) for the frequency counter as there are only 10 possible digits.
+    """
+
+    def totalNumbers(self, digits: List[int]) -> int:
+        freq = [0] * 10
+        for d in digits:
+            freq[d] += 1
+
+        res = 0
+        for n in range(100, 1000, 2):
+            d1, rem = divmod(n, 100)
+            d2, d3 = divmod(rem, 10)
+            res += freq[d1] > 0 and freq[d2] > (d1 == d2) and freq[d3] > (d1 == d3) + (d2 == d3)
 
         return res
