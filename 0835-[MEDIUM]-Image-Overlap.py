712fc11487e4115998c5a47750aa5f45210fa47f
Alexander Liu <alexander.liu22@outlook.com>
2026-09-13 17:31:20 -0400

    [835][medium] add hashing soln

--- 0835-[MEDIUM]-Image-Overlap.py
+++ 0835-[MEDIUM]-Image-Overlap.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         Brute force approach. We make a list of coordinates for
@@ -74,3 +74,62 @@ class Solution:
                 res = curr
 
         return res
+
+
+class Solution2:
+    """
+    Intuition:
+        We can optimize the brute force solution via hashing.
+
+        We can save the computation of overlap by bucketing
+        pairs of coordinates by the shift. Pairs of coordinates
+        with the same shift are grouped together and we track
+        their count.
+
+    Runtime:
+        O(n) to compute coords in img1 and img2.
+
+        O(n^2) to bucket all pairs of coords by shift.
+
+        O(n^2) to find the shift with the max overlap since
+        there are at most n^2 shifts.
+
+        Overall, O(n^2) runtime which better than O(n^3) from the
+        previous solution.
+
+    Memory:
+        O(n) for coords1 and coords2.
+
+        O(n^2) for the shifts hashmap.
+
+        Overall, O(n^2) memory.
+    """
+
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        N = len(img1)
+
+        # get all coords where cell val is 1 in img1
+        coords1 = []
+        for r in range(N):
+            for c in range(N):
+                if img1[r][c] == 1:
+                    coords1.append((r, c))
+
+        # no possible overlap
+        if not coords1:
+            return 0
+
+        coords2 = []
+        for r in range(N):
+            for c in range(N):
+                if img2[r][c] == 1:
+                    coords2.append((r, c))
+
+        # key = (dr, dc) i.e. row and col-wise cartesian dists, val = cnt
+        shifts = {}
+        for r1, c1 in coords1:
+            for r2, c2 in coords2:
+                dr, dc = r2 - r1, c2 - c1
+                shifts[(dr, dc)] = shifts.get((dr, dc), 0) + 1
+
+        return max(shifts.values()) if shifts else 0
