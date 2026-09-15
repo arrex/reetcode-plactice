da32732c7f9e4238771acf4f220a9eb491abb72d
Alexander Liu <alexander.liu22@outlook.com>
2026-09-15 15:58:33 -0400

    [2472][hard] add bottom-up dp soln

--- 2472-[HARD]-Maximum-Number-of-Non-overlapping-Palindrome-Substrings.py
+++ 2472-[HARD]-Maximum-Number-of-Non-overlapping-Palindrome-Substrings.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     """
     Intuition:
         The intuition is that we can be greedy in searching for palindromes.
@@ -56,3 +56,55 @@ class Solution:
 
         # no next pal
         return -1
+
+
+class Solution2:
+    """
+    Intuition:
+        We can use a bottom-up dp approach. Our dp cache stores solns to
+        smaller subproblems. In other words, dp[i] stores the opt soln for
+        s[:i].
+
+        Our base case dp[0] is 0 because s[:0] contains 0 palindromes. Then,
+        we iterate over each possible end index of our palindrome. We start
+        at position k since the palindrome must have len at least k and any
+        substring smaller than k will automatically contain 0 valid palindromes.
+
+        Then, we use the greedy observation to find candidates. A valid
+        palindrome in our case can either be of len k or k + 1 to account for
+        even/odd cases. If we find a match, the soln is simply the max between
+        the soln from the previous state `i - 1` and the soln from the start pos
+        `r - k` + 1.
+
+    Runtime:
+        O(n * k) since the outer loop has roughly n iterations and checking
+        each candidate requires O(k) time.
+
+    Memory:
+        O(n) for the dp cache.
+
+        O(k) auxiliary memory to check each candidate.
+
+        Overall, O(n + k) space.
+    """
+
+    def maxPalindromes(self, s: str, k: int) -> int:
+        N = len(s)
+        # dp[i] contains opt soln for s[:i]
+        # base case: dp[0] = 0
+        dp = [0] * (N + 1)
+
+        for r in range(k, N + 1):
+            dp[r] = dp[r - 1]
+
+            if r >= k:
+                candidate = s[r - k : r]
+                if candidate == candidate[::-1]:
+                    dp[r] = max(dp[r], dp[r - k] + 1)
+
+            if r >= k + 1:
+                candidate = s[r - k - 1 : r]
+                if candidate == candidate[::-1]:
+                    dp[r] = max(dp[r], dp[r - k - 1] + 1)
+
+        return dp[N]
